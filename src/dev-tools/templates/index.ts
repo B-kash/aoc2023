51320d287fcacf3cd6cee1c635fd1f7214f5dbf4
b-kash <bikashchapagain047@gmail.com>
2023-12-02 12:21:07 +0100

    ♻️ update template

--- src/dev-tools/templates/index.ts
+++ src/dev-tools/templates/index.ts
@@ -1,12 +1,20 @@
 import input from "./input.json";
 
 // TODO: parse your input here
-const parseInput = (i: any[]) => {};
+const parseInput = (i: any[]) => {
+  return i;
+};
+
+const resolvePart1 = (i: any[]) => {};
+
+const resolvePart2 = (i: any[]) => {};
 
 // main app code goes here
 const start = () => {
   const parsedInput = parseInput(input);
   console.log(parsedInput);
+  console.log("result part1: ", resolvePart1(parsedInput));
+  console.log("result part2: ", resolvePart2(parsedInput));
 };
 
 // run the app
